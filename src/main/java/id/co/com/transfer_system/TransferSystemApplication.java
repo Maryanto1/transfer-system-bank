8deb615cfe71170b9f718a27a69f3468415e1b2c
Maryanto_1 <maryantom954@gmail.com>
2024-06-13 19:49:47 +0700

    init

---
+++ src/main/java/id/co/com/transfer_system/TransferSystemApplication.java
@@ -0,0 +1,13 @@
+package id.co.com.transfer_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/com/transfer_system/TransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.com.transfer_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
